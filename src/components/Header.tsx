f1f88a2fc03e09833c927fa2014cf914a555cba5
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2022-12-08 16:14:24 -0300

    add form

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,5 +4,10 @@ import logo from "./../assets/todoLogo.svg";
 export const Header = () => (
   <header className={styles.header}>
     <img src={logo} alt="ToDo logo" loading="lazy" />
+
+    <form className={styles.newTaskForm}>
+      <input type="text" placeholder="Adicione uma nova tarefa" />
+      <button type="submit">Criar</button>
+    </form>
   </header>
 );
