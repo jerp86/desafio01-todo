3e2cb96c656f09b2baf50dde3f24168db9df3f60
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2022-12-08 18:51:49 -0300

    add functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,45 @@
+import { ChangeEvent, FormEvent, useState } from "react";
 import { PlusCircle } from "phosphor-react";
 
 import styles from "./Header.module.css";
 import logo from "./../assets/todoLogo.svg";
 
-export const Header = () => (
-  <header className={styles.header}>
-    <img src={logo} alt="ToDo logo" loading="lazy" />
+interface Props {
+  onAddTask: (value: string) => void;
+}
 
-    <form className={styles.newTaskForm}>
-      <input type="text" placeholder="Adicione uma nova tarefa" />
-      <button type="submit">
-        Criar
-        <PlusCircle size={16} />
-      </button>
-    </form>
-  </header>
-);
+export const Header = ({ onAddTask }: Props) => {
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = (evt: FormEvent) => {
+    evt.preventDefault();
+
+    if (!title || title.length === 0) return;
+
+    onAddTask(title);
+    setTitle("");
+  };
+
+  const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    setTitle(evt.target.value);
+  };
+
+  return (
+    <header className={styles.header}>
+      <img src={logo} alt="ToDo logo" loading="lazy" />
+
+      <form className={styles.newTaskForm} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Adicione uma nova tarefa"
+          value={title}
+          onChange={handleChange}
+        />
+        <button type="submit">
+          Criar
+          <PlusCircle size={16} />
+        </button>
+      </form>
+    </header>
+  );
+};
