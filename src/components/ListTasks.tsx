dabbf1964a69c3fbff775efc8935f792db6570cb
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2022-12-09 00:06:27 -0300

    add empty structure

--- src/components/ListTasks.tsx
+++ src/components/ListTasks.tsx
@@ -1,6 +1,7 @@
 import { ITask } from "../App";
 import styles from "./ListTasks.module.css";
 import { Task } from "./Task";
+import clipboard from "./../assets/clipboard.svg";
 
 interface Props {
   tasks: ITask[];
@@ -26,16 +27,31 @@ export const ListTasks = ({ tasks, onCheck, onDelete }: Props) => {
         </div>
       </header>
 
-      <div className={styles.list}>
-        {tasks.map((task) => (
-          <Task
-            key={task.id}
-            task={task}
-            onDelete={onDelete}
-            onCheck={onCheck}
+      {tasks.length === 0 ? (
+        <section className={styles.empty}>
+          <img
+            src={clipboard}
+            alt="Icon representing the action of copying"
+            loading="lazy"
           />
-        ))}
-      </div>
+
+          <div>
+            <p>Você ainda não tem tarefas cadastradas</p>
+            <span>Crie tarefas e organize seus itens a fazer</span>
+          </div>
+        </section>
+      ) : (
+        <div className={styles.list}>
+          {tasks.map((task) => (
+            <Task
+              key={task.id}
+              task={task}
+              onDelete={onDelete}
+              onCheck={onCheck}
+            />
+          ))}
+        </div>
+      )}
     </section>
   );
 };
